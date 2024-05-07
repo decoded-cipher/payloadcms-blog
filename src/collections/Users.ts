6ce5cb8cea30a8b3dccc25d5e235fe67df6e37e5
arjun.airtory <arjun12345krishna@gmail.com>
2024-05-08 04:17:56 +0530

    Users

--- src/collections/Users.ts
+++ src/collections/Users.ts
@@ -9,6 +9,49 @@ const Users: CollectionConfig = {
   fields: [
     // Email added by default
     // Add more fields as needed
+
+    {
+      name: 'name',
+      label: 'Full Name',
+      type: 'text',
+    },
+    {
+      name: 'bio',
+      label: 'Bio',
+      type: 'textarea',
+    },
+    {
+      name: 'slug',
+      label: 'Slug',
+      type: 'text',
+    },
+    {
+      name: 'role',
+      label: 'Role',
+      type: 'select',
+      options: [
+        {
+          label: 'Admin',
+          value: 'admin',
+        },
+        {
+          label: 'Editor',
+          value: 'editor',
+        },
+        {
+          label: 'Author',
+          value: 'author',
+        },
+        {
+          label: 'Contributor',
+          value: 'contributor',
+        },
+        {
+          label: 'Subscriber',
+          value: 'subscriber',
+        }
+      ],
+    }
   ],
 }
 
